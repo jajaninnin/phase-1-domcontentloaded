434442d6d650d4eb7e6f242a2e210fb1a38715ab
janine pamintuan <jaja.ninnin@gmail.com>
2024-10-21 15:33:43 -0400

    domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const textID = document.getElementById("text")
+    textID.textContent = "This is really cool!"
+})
